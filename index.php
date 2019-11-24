b09f86cb6421fd7b99af02cae039cfb8b1f21060
thuvarahan97 <thuvarahan97@gmail.com>
2019-11-25 02:53:47 +0530

    Home and Contact Info modified

---
+++ index.php
@@ -0,0 +1,145 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Once Upon A Time &mdash; Weddings & Events</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="Sivaneswaran Thuvarahan" />
+
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	DESIGNED & DEVELOPED by Sivaneswaran Thuvarahan
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+	
+	<link href="https://fonts.googleapis.com/css?family=Montez" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+	
+		<div class="fh5co-hero" data-section="home">
+			<div class="fh5co-overlay"></div>
+			<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(images/cover_bg_1.jpg);">
+				<div class="display-t">
+					<div class="display-tc">
+						<div class="container">
+							<div class="col-md-10 col-md-offset-1">
+								<div class="animate-box">
+									<h2>Once Upon A Time</h2>
+                                    <h2>Weddings &amp; Events</h2>
+                                    <h1>Provided to you by</h1>
+                                    <h1>Rowena Suaris</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="fh5co-couple" class="">
+			<div class="container">
+				<div class="row animate-box">
+					<div class="col-md-12">
+						<div class="col-md-12 text-center heading-section">
+							<h2>Mission Statement ...</h2>
+						</div>
+					</div>
+                </div>
+                
+                <div class="row animate-box">
+					<div class="col-md-12">
+						<div class="col-md-12 text-center heading-section">
+							<a href="about_me.php"><h1 class="tab_link">About Me</h1></a>
+							<a href="pricing.php"><h1 class="tab_link">Pricing</h1></a>
+							<a href="services"><h1 class="tab_link">Services</h1></a>
+							<a href="previous_weddings.php"><h1 class="tab_link">Previous Weddings</h1></a>
+							<a href="vendor_list.php"><h1 class="tab_link">Vendor List</h1></a>
+							<a href="reviews.php"><h1 class="tab_link">Reviews</h1></a>
+							<a href="contat-info.php"><h1 class="tab_link">Contact Info & Inquiries</h1></a>
+						</div>
+					</div>
+                </div>
+                
+			</div>
+        </div>
+
+		<footer>
+			<div id="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 text-center">
+							<p style="font-size:16px;">Copyright ©2019 All Rights Reserved | S.Thuvarahan</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+	
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+	<!-- jQuery -->
+	<script src="dist/scripts.min.js"></script>
+	</body>
+</html>
+
